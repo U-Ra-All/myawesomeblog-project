3185f50a5688472a61d6a881fd2bafde1ae6af3c
YouRa <allakhverdovyuriy@gmail.com>
2020-06-01 11:10:30 +0300

    Show posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def showblog(request):
+	posts = Post.objects
+	return render(request, 'blog/blog.html', {'posts': posts})
--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import events.views
@@ -22,6 +22,7 @@ import events.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', events.views.home, name='home'),
+    path('posts/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
