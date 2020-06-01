14856c88350534128ff74640a956d321e5a508e1
YouRa <allakhverdovyuriy@gmail.com>
2020-06-01 15:43:46 +0300

    Add static feature

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -126,10 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, 'myawesomeblog/static/')
+
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/awesome_media/'
 
 
